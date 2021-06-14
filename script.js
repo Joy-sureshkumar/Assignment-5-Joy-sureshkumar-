df86d8d11e609560c37da5fce80bf37d9d0b1ca9
Joy Sureshkumar <84517105+Joy-sureshkumar@users.noreply.github.com>
2021-06-14 00:37:49 +0000

    new

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let diameter = 0
-let radius = 0
 let area = 0
 let circumference = 0
 const PI = 22 / 7
@@ -7,18 +6,19 @@ const PI = 22 / 7
 document.getElementById('button').addEventListener('click', areaOfCircle)
 document.getElementById('button').addEventListener('click', circumferenceOfCircle)
 
-//area of a circle = (d / 2)^2 * 3.14
+//area of a circle = (d / 2)^2 * PI
 function areaOfCircle() {
- diameter = document.getElementById('diameter').value
-diameter = parseInt(diameter)
-result = ((diameter / 2) ** 2) * PI;
-document.getElementById('area').innerHTML = result
+  diameter = document.getElementById('diameter').value
+  diameter = parseInt(diameter)
+  result = ((diameter / 2) ** 2) * PI;
+  document.getElementById('area').innerHTML = result
 }
 
 //circumference of a circle = PI * diameter
 function circumferenceOfCircle() {
- diameter = document.getElementById('diameter').value
-diameter = parseInt(diameter)
-result = PI * diameter;
-document.getElementById('circumference').innerHTML = result
+  diameter = document.getElementById('diameter').value
+  diameter = parseInt(diameter)
+  result = PI * diameter;
+  document.getElementById('circumference').innerHTML = result
+}
 }
